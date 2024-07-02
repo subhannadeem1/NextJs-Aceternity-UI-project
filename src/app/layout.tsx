8bde8d36eb1c210872f0b040a067e1902594d756
subhan1212 <70110442@student.uol.edu.pk>
2024-07-02 13:49:14 +0500

    1st commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
+import { ColorSchemeScript, MantineProvider } from "@mantine/core";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +17,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className="dark">
+      <head>
+     
+      </head>
+      <body className={`dark:bg-black text-white ${inter.className}`}> 
+      <MantineProvider >{children}</MantineProvider>
+        <Navbar />
+        
+        </body>
     </html>
   );
 }
